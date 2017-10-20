909792a17351ab2d9a3239eac51ae5593fb0112c
egin10 <wweusz@gmail.com>
2017-10-20 22:07:00 +0700

    initial commit

---
+++ layouts/header_body.php
@@ -0,0 +1,10 @@
+<header class="masthead">
+    <div class="container">
+    <img class="img-fluid" src="img/profile.jpg" alt="" width="200px" height="200px" style="border-radius:100px;">
+    <div class="intro-text">
+        <span class="name">DevCat</span>
+        <hr class="star-light">
+        <span class="skills">Web Developer - Backend Programmer</span>
+    </div>
+    </div>
+</header>
